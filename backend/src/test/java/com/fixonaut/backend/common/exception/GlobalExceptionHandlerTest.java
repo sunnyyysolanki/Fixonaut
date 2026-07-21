d1848073a9aff0de8a434c005585124b246f65a2
Sunny Solanki <sunnysolanki74860@gmail.com>
2026-07-21 12:17:12 +0530

    feat: add Spring Security foundation

--- backend/src/test/java/com/fixonaut/backend/common/exception/GlobalExceptionHandlerTest.java
+++ backend/src/test/java/com/fixonaut/backend/common/exception/GlobalExceptionHandlerTest.java
@@ -11,7 +11,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
 @WebMvcTest(controllers = ValidationTestController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @Import(GlobalExceptionHandler.class)
 class GlobalExceptionHandlerTest {
 
